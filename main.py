0cc892b32c9973d007b0d2d02a60eb7eb3870a7a
Cezidev <serupaindah665@gmail.com>
2025-12-31 15:31:32 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, jsonify
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/api/session')
+def get_session():
+    return jsonify({"status": "Active", "server": "Titanium-v13"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+  
